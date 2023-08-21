537b786f16c176d92b249b0128507ce0ca8e1fcd
orkait <orkaitsolutions@gmail.com>
2023-08-22 00:45:00 +0530

    updated topological sorting code

--- graphs_concept/topological_sorting.cpp
+++ graphs_concept/topological_sorting.cpp
@@ -19,7 +19,7 @@ class Solution {
         // you have to think in terms of recursion
         // during recursion child nodes enter ho chuka h single parent ke liye
         // next time parent ko bhi enter karwana padega stack me
-        // Also, we are pushing parent after all children since stack reverses the order
+        // we are pushing parent after all children since stack reverses the order when we pop it
         st.push(u);
     }
     vector<int> topoSort(int V, vector<int> adj[]) {
@@ -28,7 +28,7 @@ class Solution {
 
         for (int i = 0; i < V; i++) {
             if (!visited[i]) {
-                DFS(adj, i);
+                DFS(i, adj);
             }
         }
 
@@ -42,12 +42,15 @@ class Solution {
 };
 
 int main() {
-    Solution sol;
-    vector<int> adj[5] = {{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 1}};
-    vector<int> ans = sol.topoSort(5, adj);
+    int V = 6;
+    vector<int> adj[V] = {{1, 2}, {2, 3}, {4}, {4, 5}, {}, {}};
 
-    for (int &i : ans) {
-        cout << i << " ";
+    Solution sol;
+    vector<int> result = sol.topoSort(V, adj);
+
+    cout << "Topological Sorting Order: ";
+    for (int node : result) {
+        cout << node << " ";
     }
 
     return 0;
