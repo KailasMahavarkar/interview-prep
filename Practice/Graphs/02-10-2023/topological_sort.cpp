e98fe39150f36d14d6d0532bbfb184b6f10531fc
KailasMahavarkar <kailashmahavarkar5@gmail.com>
2023-10-02 03:54:20 +0530

    sync update

---
+++ Practice/Graphs/02-10-2023/topological_sort.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    stack<int> st;
+    vector<bool> visited;
+    void DFS(int curr, unordered_map<int, vector<int>> &adj) {
+        visited[curr] = true;
+
+        for (auto &x : adj[curr]) {
+            if (!visited[x]) {
+                DFS(x, adj);
+            }
+        }
+
+        // push all childs first -> DFS gurantees this
+        // then push parent
+        st.push(curr);
+    }
+    vector<int> topoSort(int V, unordered_map<int, vector<int>> &adj) {
+        visited.resize(V, false);
+        vector<int> result;
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                DFS(i, adj);
+            }
+        }
+
+        while (!st.empty()) {
+            result.push_back(st.top());
+            st.pop();
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<vector<int>> arr = {{1}, {0, 2, 4}, {1, 3}, {2, 4}, {1, 3}};
+    unordered_map<int, vector<int>> adj;
+
+    int V = arr.size();
+
+    for (int i = 0; i < V; i++) {
+        for (int &x : arr[i]) {
+            adj[i].push_back(x);
+        }
+    }
+
+    vector<int> answer = sol.topoSort(V, adj);
+
+    cout << "Topological Sorting Order: ";
+    for (int &node : answer) {
+        cout << node << " ";
+    }
+}
--- graphs_questions/alien_dictionary_leetcode_GFG.cpp
+++ graphs_questions/alien_dictionary_leetcode_GFG.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Approach 1: Kahn's Algorithm
 class Solution {
    public:
     vector<int> kahnsAlgorithm(unordered_map<int, vector<int>> &adj) {
@@ -63,6 +64,70 @@ class Solution {
         }
 
         vector<int> result = kahnsAlgorithm(adj);
+        string finalResult = "";
+
+        for (int &node : result) {
+            finalResult += (node + 'a');
+        }
+
+        return finalResult;
+    }
+};
+
+
+// Approach 2: DFS (topological sort)
+class SolutionDFS {
+   public:
+    stack<int> st;
+    vector<bool> visited;
+    void DFS(int curr, unordered_map<int, vector<int>> &adj) {
+        visited[curr] = true;
+
+        for (auto &x : adj[curr]) {
+            if (!visited[x]) {
+                DFS(x, adj);
+            }
+        }
+
+        // push all childs first -> DFS gurantees this
+        // then push parent
+        st.push(curr);
+    }
+    vector<int> topoSort(int V, unordered_map<int, vector<int>> &adj) {
+        visited.resize(V, false);
+        vector<int> result;
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                DFS(i, adj);
+            }
+        }
+
+        while (!st.empty()) {
+            result.push_back(st.top());
+            st.pop();
+        }
+
+        return result;
+    }
+
+    string findOrder(string dict[], int N, int K) {
+        unordered_map<int, vector<int>> adj;
+
+        for (int i = 0; i < N - 1; i++) {
+            string word1 = dict[i];
+            string word2 = dict[i + 1];
+
+            // compare word1 and word2
+            for (int j = 0; j < min(word1.size(), word2.size()); j++) {
+                if (word1[j] != word2[j]) {
+                    adj[word1[j] - 'a'].push_back(word2[j] - 'a');
+                    break;
+                }
+            }
+        }
+
+        vector<int> result = topoSort(K, adj);
         string finalResult = "";
 
         for (int &node : result) {
