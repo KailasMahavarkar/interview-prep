c553b04606728828d179372e5d07fd767991e2db
cw-kailas <kailas.m@carwale.com>
2024-02-12 12:57:30 +0530

    sync update

---
+++ leetcode/perfect_squares_leetcode_279.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    Problem Link   : https://leetcode.com/problems/perfect-squares/description
+    Company Tags   : None
+    Youtube Link   : None
+    Custom  Link   : None
+*/
+
+// Approach-1 (Normal)
+// TC : O()
+// SC : O()
+
+class Solution {
+   public:
+    int r;
+    vector<int> dp;
+    int solve(int idx) {
+        if (idx == 3) {
+            return 2;
+        }
+
+        if (idx <= 2) {
+            return 1;
+        }
+
+        if (dp[idx] != -1){
+            return dp[idx];
+        }
+
+        int min_value = INT_MAX;
+        for (int i = 1; i < 100; i++) {
+            int diff = idx - pow(i, 2);
+            if (diff > 0) {
+                min_value = min(min_value, 1 + solve(diff));
+            }
+        }
+
+        return dp[idx] = min_value;
+    }
+
+    int numSquares(int n) {
+        r = n;
+        dp.resize(n + 1, -1);
+        return solve(n);
+    }
+};
+
+int main() {
+    Solution sol;
+    int ans = sol.numSquares(81);
+    cout << "ans --> " << ans << endl;
+}
