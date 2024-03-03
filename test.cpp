6cc4b113634e91f780b9652e6fd06258343d7602
KailasMahavarkar <kailashmahavarkar5@gmail.com>
2024-03-03 12:36:36 +0530

    sync update

--- test.cpp
+++ test.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int main() {
-    string currString = "abcd";
+    for (int i = 0; i < 5; i++) {
+        for (int j = i + 1; j < 5; j++) {
+            for(int k = j+1; k < 5; k++){
+                if (nums[i] < nums[j] && nums[j] < nums[k]){
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
 
+    string currString = "abcd";
     cout << currString.substr(0, 3) << endl;
 }
