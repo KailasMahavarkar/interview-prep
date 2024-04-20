2ded35bbb48adae1130f004015f93082d4f7c563
orkait <orkaitsolutions@gmail.com>
2024-04-20 21:35:26 +0530

    sync update

---
+++ Bit Manipulation Concept/bitset.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    int bitsetPuzzle(vector<int>& arr) {
+        bitset<8> bs;
+
+        bs[1] = 1;
+        bs[4] = 1;
+        bs[5] = 1;
+
+        // bitset to int
+        int ans = (int)bs.to_ulong();
+        cout << "ans --> " << ans << endl;
+
+        // int to bitset
+        int num = 50;
+        bs = num;
+
+        cout << "new bitset -->" << bs << endl;
+
+        // bitset to int
+        int ans2 = (int)bs.to_ulong();
+        cout << "ans2 --> " << ans2 << endl;
+
+        vector<bitset<8>> v;
+        v.push_back(bs);
+
+        return -1;
+    }
+};
+
+int main() {
+    vector<int> arr = {4, 3, 5, 2};
+    Solution sol;
+    int ans = sol.bitsetPuzzle(arr);
+    cout << "ans --> " << ans << endl;
+}
---
+++ DP Question/maximal_square_leetcode_221.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    Problem Link   : https://leetcode.com/problems/maximal-square/
+    Company Tags   : None
+    Youtube Link   : None
+    Custom  Link   : None
+    Similarities   : None
+*/
+
+// Approach-1 (Normal -> dp)
+// TC : O(rows * cols)
+// SC : O(rows * cols)
+
+// Question: can we avoid (i != 0 && j != 0) check?
+// We can avoid this condition if we do bound check conditionally later
+// for{i->0 to rows}
+//    for(j->0 to cols)
+//       isSafe(i, j-1) ? t[i][j-1]: 0;
+
+// Question: what if we do loop like (i->1 to rows) will this still work?
+// NO, it will throw out of bound error while accessing 
+// for{i->1 to rows}
+//    for(j->1 to cols)
+
+
+class Solution {
+   public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int side = 0;
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<vector<int>> t(rows, vector<int>(cols, 0));
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                t[i][j] = matrix[i][j] - '0';
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1' && i != 0 && j != 0) {
+                    t[i][j] = 1 + min({t[i][j - 1], t[i - 1][j], t[i - 1][j - 1]});
+                }
+                side = max(side, t[i][j]);
+            }
+        }
+
+        return side * side;
+    }
+};
+
+int main() {
+    vector<vector<char>> arr = {
+        {'1', '0', '1', '0', '0'},
+        {'1', '0', '1', '1', '1'},
+        {'1', '1', '1', '1', '1'},
+        {'1', '0', '0', '1', '0'}};
+
+    Solution sol;
+    int ans = sol.maximalSquare(arr);
+    cout << "ans -->" << ans << endl;
+}
