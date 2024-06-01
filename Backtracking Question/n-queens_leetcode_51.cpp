996cc36ef4a45a78fddce38a6cf4acbc85ab150e
cw-kailas <kailas.m@carwale.com>
2024-06-01 14:53:08 +0530

    sync update

--- Backtracking Question/n-queens_leetcode_51.cpp
+++ Backtracking Question/n-queens_leetcode_51.cpp
@@ -16,21 +16,99 @@ using namespace std;
 
 class Solution {
    public:
-    vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> answer;
+    int N;
+    vector<vector<string>> result;
+    vector<pair<int, int>> directions = {
+        {-1, 2},
+        {-1, -2},
+        {-2, -1},
+        {-2, 1},
+        {1, 2},
+        {1, -2},
+        {2, -1},
+        {2, 1}};
 
-        return answer;
+    set<int> occupied_row;
+    set<int> occupied_col;
+
+    bool isSafe(int x, int y, int row, int col) {
+        bool isBoundaryValid = x >= 0 && x < N && y >= 0 && y < N;
+        if (!isBoundaryValid) {
+            return false;
+        }
+
+        // left diagonal
+        for (int i = 0; i < N; i++) {
+            
+        }
+
+        // right diagonal
+
+        // rows
+
+        // cols
+    }
+
+    void solve(int n, int x, int y, vector<string> &temp) {
+        if (n == 0) {
+            result.push_back(temp);
+        }
+
+        temp[x][y] = 'Q';
+
+        for (auto &dir : directions) {
+            int _x = x + dir.first;
+            int _y = y + dir.second;
+
+            if (isSafe(_x, _y) && temp[_x][_y] == '.') {
+                solve(n - 1, _x, _y, temp);
+            }
+        }
+
+        temp[x][y] = '.';
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        N = n;
+
+        // create a nxn grid
+        vector<string> temp;
+        for (int i = 0; i < n; i++) {
+            string s = "";
+            for (int j = 0; j < n; j++) {
+                s += '.';
+            }
+            temp.push_back(s);
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                solve(n, i, j, temp);
+            }
+        }
+
+        return result;
     }
 };
 
 int main() {
     Solution sol;
-    vector<vector<string>> ans = sol.solveNQueens(10);
+    vector<vector<string>> ans = sol.solveNQueens(4);
 
-    for (auto x : ans) {
-        for (auto ch : x) {
-            cout << ch << " ";
+    for (auto combinationVec : ans) {
+        for (auto combination : combinationVec) {
+            for (auto &line : combination) {
+                cout << line << " ";
+            }
+            cout << endl;
         }
         cout << endl;
     }
+
+    // for (auto x : ans) {
+    //     for (auto ch : x) {
+    //         cout << ch << " ";
+    //     }
+    //     cout << endl;
+    // }
 }
---
+++ Backtracking Question/nqueen_safe.py
@@ -0,0 +1,49 @@
+board = [
+    ". . . . .",  # 0
+    ". . . Q .",  # 1
+    ". . . . .",  # 2
+    ". . . . .",  # 3
+    ". . . . ."  # 4
+]
+N = len(board)
+
+
+def isSafe(board, row, col):
+    # check if there is only one queen in the row
+    if board[row].count("Q") > 1:
+        return False
+
+    # check if there is a queen in the same column
+    if [x[col] for x in board].count("Q") > 1:
+        return False
+
+
+    i = row
+    j = col
+    while i >= 0 and j >= 0:
+        if (board[i][j] == 'Q'):
+            return False
+        i -= 1
+        j -= 1
+
+    i = row
+    j = col
+    while i >= 0 and j < N:
+        if (board[i][j] == 'Q'):
+            return False
+        i -= 1
+        j += 1
+
+    return True
+
+
+# (1, 0) (2, 1) (3, 2), (4, 3)
+board = [x.replace(" ", "") for x in board]
+
+print(
+    isSafe(board=board, row=4, col=0)
+)
+# print(board[1][0])
+# print(board[2][1])
+# print(board[3][2])
+# print(board[4][3])
