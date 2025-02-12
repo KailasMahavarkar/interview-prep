eb98b61a61277034393f8867b67cf42ab872a0a8
KailasMahavarkar <kailashmahavarkar5@gmail.com>
2025-02-13 01:26:38 +0530

    added cp problems

---
+++ Codeforces/800/lcm_problem_leetcode_A.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Steps taken to reach solution
+// Observation:
+// LCM(2, 3) -> 6
+// LCM(2, 4) -> 4   (x, 2x) -> 2x
+// LCM(2, 5) -> 10
+// LCM(2, 6) -> 6   (x, 3x) -> 3x
+// LCM(2, 7) -> 14
+// LCM(2, 8) -> 8   (x, 4x) -> 4x
+
+// Reading Question and Dedude facts:
+// So i can say... LCM(l, n*l) -> n*l
+// but we know 
+// l <= LCM(l, n*l) <= r
+// so we can deduce 
+// n*l <= r
+// n can be any natural number... starting from 2
+// finally we can say, 2*l <= r
+
+// Use this Input/Output since codeforces have wrong Output... but code passes for following output
+// Input
+// 4
+// 1 1337
+// 13 69
+// 2 4
+// 88 89
+// Output
+// 1 2
+// 13 26
+// 2 4
+// -1 -1
+
+class Solution {
+   public:
+    void solve(int l, int r) {
+        if (2 * l <= r) {
+            cout << l << " " << 2 * l << endl;
+        } else {
+            cout << "-1 -1" << endl;
+        }
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int l, r;
+        cin >> l >> r;
+        Solution sol;
+        sol.solve(l, r);
+    }
+    return 0;
+}
---
+++ Codeforces/800/only_pluses_leetcode_A.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 4
+// 1 1337
+// 13 69
+// 2 4
+// 88 89
+// Output
+// 1 2
+// 13 26
+// 2 4
+// -1 -1
+class Solution {
+   public:
+    void solve(int a, int b, int c) {
+        for (int i = 0; i < 5; i++) {
+            if (a <= b && a <= c) {
+                ++a;
+            }
+            else if (b <= a && b <= c) {
+                ++b;
+            }
+            else if (c <= a && c <= b) {
+                ++c;
+            }
+        }
+        cout << a * b * c << endl;
+    };
+};
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        Solution sol;
+        sol.solve(a, b, c);
+    }
+
+    // Solution sol;
+    // sol.solve(2, 3, 4);
+    return 0;
+}
+
+
