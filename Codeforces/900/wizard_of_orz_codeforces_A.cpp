84f7eddb4896cdbeeb94164ed8f9abebf69137c7
orkait <66670953+KailasMahavarkar@users.noreply.github.com>
2025-03-23 05:20:56 +0530

    sync update

--- Codeforces/900/wizard_of_orz_codeforces_A.cpp
+++ Codeforces/900/wizard_of_orz_codeforces_A.cpp
@@ -7,6 +7,35 @@ class Solution {
     void solve(int n) {
         ll k = 9;
 
+        // i = 0 | k = 9
+        // print 9
+        // decrement k
+
+        // i = 1 | k = 8
+        // print 8
+        // increment k
+
+        // i = 2 | k = 9
+        // print 9
+        // increment k
+
+        // i = 3 | k = 10
+        // reset k = 0
+
+        // i = 4 | k = 0
+        // print 0
+        // increment k
+
+        // i = 5 | k = 1
+        // print 1
+        // increment k...
+
+        // i = 6 | k = 2
+        // print 2
+        // increment k...
+
+        // pattern as: 989012....
+
         for (ll i = 0; i < n; i++) {
             if (i == 0) {
                 cout << k;
@@ -23,15 +52,16 @@ class Solution {
 };
 
 int main() {
+    Solution sol;
     int t, n;
     cin >> t;
-
-    Solution sol;
 
     while (t--) {
         cin >> n;
         sol.solve(n);
     }
+
+    // sol.solve(30);
 
     return 0;
 }
